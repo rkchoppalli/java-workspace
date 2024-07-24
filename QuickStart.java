11c5e5cab87f0768087ccc1687abf2e9545d3e9d
Ravi Choppalli <rkchoppalli@gmail.com>
2024-07-24 01:37:26 -0400

    first branch publish

---
+++ QuickStart.java
@@ -0,0 +1,7 @@
+class QuickStart {
+    public static void main (String[] args) {
+        int num = 123;
+        String str = "Hello World";
+        System.out.println("Number is :"+num+"  and String is:"+str) ;
+    }
+}
