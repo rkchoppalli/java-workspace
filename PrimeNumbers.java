aa551d35a1520a265ba14c22bad5c53c2753fffb
Ravi Choppalli <rkchoppalli@gmail.com>
2024-07-24 02:02:24 -0400

    added primes

---
+++ PrimeNumbers.java
@@ -0,0 +1,33 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int n = 100; // Upper limit for finding prime numbers
+
+        System.out.println("Prime numbers up to " + n + ":");
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num <= 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
